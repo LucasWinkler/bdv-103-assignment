2d4f6265e9c8180bd45930054723944f5553484d
Lucas Winkler <lucaswinkler@gmail.com>
2025-07-01 19:18:49 +0000

    Revert "Test"
    This reverts commit 3a1cc41218b3df77b80d71ad8c76fca6a11226c1.

--- database_test_setup.ts
+++ database_test_setup.ts
@@ -1,43 +1,22 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import { afterAll, beforeAll } from 'vitest'
 
-let instance: MongoMemoryServer | null = null
-
 export async function setup (): Promise<void> {
-  try {
-    instance = await MongoMemoryServer.create({
-      binary: {
-        version: '7.0.7',
-        downloadDir: '/tmp/mongodb-binaries'
-      },
-      instance: {
-        dbName: 'test-db'
-      }
-    })
-
-    while (instance.state === 'new') {
-      await instance.start()
-    }
-
-    const uri = instance.getUri()
-    ;(global as any).__MONGOINSTANCE = instance
-    ;(global as any).MONGO_URI = uri.slice(0, uri.lastIndexOf('/'))
-  } catch (error) {
-    console.error('Failed to setup MongoDB Memory Server:', error)
-    throw error
+  const instance = await MongoMemoryServer.create({ binary: { version: '7.0.7' } })
+  while (instance.state === 'new') {
+    await instance.start()
   }
+  const uri = instance.getUri();
+  (global as any).__MONGOINSTANCE = instance;
+  (global as any).MONGO_URI = uri.slice(0, uri.lastIndexOf('/'))
 }
 
 export async function teardown (): Promise<void> {
-  try {
-    if (instance !== null) {
-      await instance.stop({ doCleanup: true })
-      instance = null
-    }
-  } catch (error) {
-    console.error('Failed to teardown MongoDB Memory Server:', error)
+  const instance: MongoMemoryServer = (global as any).__MONGOINSTANCE
+  if (instance !== undefined) {
+    await instance.stop({ doCleanup: true })
   }
 }
 
-beforeAll(setup, 30000)
-afterAll(teardown, 10000)
+beforeAll(setup)
+afterAll(teardown)
