ae6115526eeafab488a132e86ed63f35eee8731e
Lucas Winkler <lucaswinkler@gmail.com>
2025-07-01 18:18:33 +0000

    Fix eslintrc error

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": "standard-with-typescript",
+    "ignorePatterns": [".eslintrc.cjs"],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "./tsconfig.json"
+    },
+    "rules": {
+    }
+}
