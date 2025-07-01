a417219b8fc178ee567308e4de975e4b76277b56
Lucas Winkler <lucaswinkler@gmail.com>
2025-07-01 19:24:15 +0000

    Revert "Test"
    This reverts commit c313409ce5fad9df4e864210b1bb68f8a8101047.

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   test: {
     includeSource: ['src/**/*.{js,ts}'],
     setupFiles: ['./database_test_setup.ts'],
-    globalSetup: './global_database_install.ts',
     testTimeout: 10000,
     hookTimeout: 15000,
     teardownTimeout: 10000
