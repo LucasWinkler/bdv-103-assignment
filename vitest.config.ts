72361bbf8e78e00efd7dc39bb66810b595a233a2
Lucas Winkler <lucaswinkler@gmail.com>
2025-07-01 03:03:55 +0000

    Setup openapi client generation and update ci workflow

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vitest/config'
+import vitestOpenapiPlugin from './vitest-openapi-plugin'
 
 export default defineConfig({
   test: {
     includeSource: ['src/**/*.{js,ts}'],
     setupFiles: ['./database_test_setup.ts']
+  },
+  plugins: [vitestOpenapiPlugin],
+  server: {
+    watch: {
+      ignored: ['build/**', 'client/**']
+    }
   }
 })
